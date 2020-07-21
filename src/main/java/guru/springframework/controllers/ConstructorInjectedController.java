3bb175ff6a524aa62d3a6f3798e00881028b5de9
teodor.miu <teodor.miu@rcs-rds.ro>
2020-07-21 22:23:59 +0300

    dependency injection with spring

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,15 +1,17 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 	private GreetingService greetingService;
 
-	public ConstructorInjectedController(GreetingService greetingService){
+	public ConstructorInjectedController(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
 
-	public String sayHello(){
+	public String sayHello() {
 		return this.greetingService.sayGreeting();
 	}
 }
--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -1,5 +1,8 @@
 package guru.springframework.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
 	public static final String GREETING = "hello from GreetingServiceImpl";
 
